19b722185ea52deec8bf89b26d1108749ecc3fbe
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-27 02:23:48 +0200

    .

--- Kapitel_10/wischen_schieben_ziehen/android/app/src/main/kotlin/com/example/wischen_schieben_ziehen/MainActivity.kt
+++ Kapitel_10/wischen_schieben_ziehen/android/app/src/main/kotlin/com/example/wischen_schieben_ziehen/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.wischen_schieben_ziehen
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
