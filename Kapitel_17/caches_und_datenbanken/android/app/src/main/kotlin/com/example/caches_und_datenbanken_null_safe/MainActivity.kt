48b969f5e742d962491f96af193756cb6dbd66dd
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-27 03:29:57 +0200

    .

--- Kapitel_17/caches_und_datenbanken/android/app/src/main/kotlin/com/example/caches_und_datenbanken_null_safe/MainActivity.kt
+++ Kapitel_17/caches_und_datenbanken/android/app/src/main/kotlin/com/example/caches_und_datenbanken_null_safe/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.caches_und_datenbanken_null_safe
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
