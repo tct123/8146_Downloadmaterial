d7c441f4fcc10e05e59ddf7666f2200f29963f3e
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-25 06:54:15 +0200

    .

--- Kapitel_18/hardware_null_safe/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Kapitel_18/hardware_null_safe/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import connectivity_macos
 import file_selector_macos
 import geolocator_apple
+import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
 }
