f1bd64a557607ba5da2ac5e3cde4f6a4f29a84e6
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-25 07:05:54 +0200

    Update upgrade_deps.py

--- upgrade_deps.py
+++ upgrade_deps.py
@@ -6,14 +6,16 @@ path = "."
 take = 0
 first_dir_list = os.listdir()
 dir_list = []
+
 for dir in first_dir_list:
     if "." in dir:
         pass
     else:
         dir_list.append(dir)
-for dir in dir_list:
-    print(dir)
-    subprocess.run(["cd", dir])
-    subprocess.run(["flutter", "pub", "upgrade", "--major-versions"])
-
-# print(dir_list)
+    print(dir_list)
+    l = os.chdir(dir)
+    print(l)
+    # for dir in dir_list:
+    #    print(dir)
+    #    subprocess.run(["cd", dir])
+    #    # subprocess.run(["flutter", "pub", "upgrade", "--major-versions"])
